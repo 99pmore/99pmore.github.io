9fb5a954431818fc34ba75c073cc87db1d37e347
99pmore <morenomartin.pablo@gmail.com>
2022-12-12 00:20:08 +0100

    starting about me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
