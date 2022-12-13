029ea370237017db3f888231a0dc9dc15d58e3f7
99pmore <morenomartin.pablo@gmail.com>
2022-12-13 18:09:37 +0100

    skills route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './pages/about/about.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: HeaderComponent},
   { path: 'about', component: AboutComponent},
+  { path: 'skills', component: SkillsComponent},
   { path: '**', pathMatch: 'full', redirectTo: ''}
 ];
 
