17bb6536eb49640f291d23d326f34d7d16927f98
99pmore <morenomartin.pablo@gmail.com>
2022-12-20 11:24:38 +0100

    cv download button

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -12,4 +12,12 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  downloadFile() {
+    const fileUrl = '../../../assets/docs/Pablo Moreno Martín CV.pdf';
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'CV_Pablo_Moreno_Martin.pdf';
+    link.click();
+  }
+
 }
